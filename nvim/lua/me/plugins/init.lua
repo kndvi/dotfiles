31b4da1f6794b588f6f71173ce7135ee3f4c9db1
Khoa Nguyen <mail@khoand.net>
2026-07-13 13:54:48 +0700

    revert keymaps

--- nvim/lua/me/plugins/init.lua
+++ nvim/lua/me/plugins/init.lua
@@ -1,7 +1,7 @@
 -- keep things simple here; only essentials
 vim.pack.add({
     "https://github.com/mhinz/vim-signify",
-    "https://codeberg.org/mfussenegger/nvim-dap",
+    "https://codeberg.org/mfussenegger/nvim-dap"
 })
 -- config for plugins
 require("me.plugins.dap")
