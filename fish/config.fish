2135d4093d092c2d86cfab327c337b8f1cbc5836
Khoa Nguyen <mail@khoand.net>
2026-07-13 13:54:39 +0700

    simplify

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 set fish_greeting
 if status is-interactive
+    fish_vi_key_bindings
+
     set -gx XDG_CONFIG_HOME "$HOME/.config"
     set -gx XDG_DATA_HOME "$HOME/.local/share"
     set -gx XDG_CACHE_HOME "$HOME/.cache"
