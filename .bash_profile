e60d1b67490ef5588a7d785e9b192b754071c6c7
Khoa Nguyen <mail@khoand.net>
2026-07-13 13:54:34 +0700

    fish

--- .bash_profile
+++
@@ -1,2 +0,0 @@
-[[ -f "${HOME}/.bashrc" ]] && source "${HOME}/.bashrc"
-[[ -s "${HOME}/.profile" ]] && source "${HOME}/.profile"
---
+++ config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    set -gx XDG_CONFIG_HOME "$HOME/.config"
+    set -gx XDG_DATA_HOME "$HOME/.local/share"
+    set -gx XDG_CACHE_HOME "$HOME/.cache"
+    set -gx RIPGREP_CONFIG_PATH "$HOME/.ripgreprc"
+
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim
+    # SDL_VIDEO_DRIVER=wayland SDL_VIDEO_WAYLAND_SCALE_TO_DISPLAY=1
+
+    alias vi=nvim
+    alias myip="echo (dig +short txt ch whoami. cloudflare @1.0.0.1)"
+end
