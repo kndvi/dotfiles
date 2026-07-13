55c6622af1370de378d4465d2a4affb4c2b05880
Khoa Nguyen <mail@khoand.net>
2026-07-13 13:54:42 +0700

    abbrs

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,18 @@ if status is-interactive
     set -gx VISUAL nvim
     set -gx RIPGREP_CONFIG_PATH "$HOME/.ripgreprc"
     set -gx PATH "$HOME/.local/bin" $PATH
-    # SDL_VIDEO_DRIVER=wayland SDL_VIDEO_WAYLAND_SCALE_TO_DISPLAY=1
+
+    abbr -a -- - 'cd -'
+    abbr -a -- gs 'git status'
+    abbr -a -- gco 'git checkout'
+    abbr -a -- gd 'git diff'
+    abbr -a -- gf 'git fetch'
+    abbr -a -- gp 'git pull'
+    abbr -a -- gP 'git push'
+    abbr -a -- ga 'git add'
+    abbr -a -- gaa 'git add --all'
+    abbr -a -- gap 'git add --patch'
+    abbr -a -- gc 'git commit -m'
 
     alias vi=nvim
     alias myip='echo (dig +short txt ch whoami.cloudflare @1.0.0.1)'
