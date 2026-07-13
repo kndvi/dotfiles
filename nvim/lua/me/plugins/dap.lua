10bbae94f7c18f200a93dc22cd49957cc122bbb9
Khoa Nguyen <mail@khoand.net>
2026-07-13 13:54:37 +0700

    dap keymaps

--- nvim/lua/me/plugins/dap.lua
+++ nvim/lua/me/plugins/dap.lua
@@ -1,25 +1,45 @@
 local utils = require("me.utils")
 local dap = require("dap")
+local map = vim.keymap.set
+
+map("n", "<Space>db", dap.toggle_breakpoint)
+map("n", "<Up>", dap.continue)
+map("n", "<Down>", dap.step_into)
+map("n", "<Right>", dap.step_over)
+map("n", "<Left>", dap.step_out)
+map("n", "<Space>dr", function() dap.repl.open({ height = 15 }) end)
 
 dap.adapters.java = function(callback)
     local client, buf = utils.get_current_client("jdtls", 5000)
-    client:request("workspace/executeCommand", { command = "vscode.java.startDebugSession" }, function(err, port)
-        assert(not err, vim.inspect(err))
-        callback({
-            type = "server",
-            host = "127.0.0.1",
-            port = port
-        })
-    end, buf)
+    client:request("workspace/executeCommand",
+        { command = "vscode.java.startDebugSession" },
+        function(err, port)
+            assert(not err, vim.inspect(err))
+            callback({
+                type = "server",
+                host = "127.0.0.1",
+                port = port
+            })
+        end, buf)
 end
 
+-- just general use cases
+-- project specific configs are located in launch.json
 dap.configurations.java = {
     {
         type = "java",
         request = "attach",
-        name = "Debug (Attach) - Remote",
-        hostName = "127.0.0.1",
+        name = "Java Debug (Attach) - Local 5055",
+        hostName = "localhost",
+        port = 5055,
+        timeout = 5000
+    },
+    {
+        type = "java",
+        request = "attach",
+        name = "Java Debug (Attach) - Remote 5005",
+        hostName = "localhost",
         port = 5005,
         timeout = 30000
-    },
+    }
 }
