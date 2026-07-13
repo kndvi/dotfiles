d0dea00b1dcc35ddee778993080904161b3a6a0d
Khoa Nguyen <mail@khoand.net>
2026-07-13 13:54:40 +0700

    treesitter

--- nvim/lua/me/plugins/init.lua
+++ nvim/lua/me/plugins/init.lua
@@ -3,8 +3,12 @@ vim.pack.add({
     "https://github.com/sainnhe/gruvbox-material",
     "https://github.com/tpope/vim-surround",
     "https://github.com/mhinz/vim-signify",
-    "https://github.com/mfussenegger/nvim-dap"
+    "https://github.com/mfussenegger/nvim-dap",
+    "https://github.com/nvim-treesitter/nvim-treesitter"
 })
 
 -- config for plugins
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_better_performance = 1
 require("me.plugins.dap")
+require("me.plugins.treesitter")
---
+++ nvim/lua/me/plugins/treesitter.lua
@@ -0,0 +1,4 @@
+require("nvim-treesitter").install({
+    "c", "cpp", "zig", "java", "python", "lua", "javascript", "typescript", "bash",
+    "fish", "json", "markdown", "diff", "sql", "html", "xml", "clojure", "terraform"
+})
